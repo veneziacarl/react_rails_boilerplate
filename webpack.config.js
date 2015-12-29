335b8338e5a0148fcc0ef853612b88d828a6bbf3
Carl Venezia <veneziacarl@gmail.com>
2015-12-29 10:14:16 -0500

    successfully implemented in to asset pipeline with JSX+ES6 loader transform

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './client/components/entry.jsx',
-  output: { path: __dirname, filename: 'bundle.js' },
+  entry: './client/entry.jsx',
+  output: { path: __dirname + '/app/assets/javascripts', filename: 'bundle.js' },
   module: {
     loaders: [
       {
         test: /.jsx?$/,
-        loader: 'babel-loader',
+        loader: ['babel-loader'],
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react']
